79b6b607dbc8433d7c6552d89ccab6dd1ae58113
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 21:55:30 +0530

    Add Fertilizer model with All column

---
+++ Models/Fertilizer.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fertilizer360.Models
+{
+    [Table("fertilizer")]
+    public class Fertilizer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("shops_id")]
+        public int ShopId { get; set; }
+
+        [ForeignKey("ShopId")]
+        public virtual Shop? Shop { get; set; }
+
+        [Required, StringLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public int Stocks { get; set; }
+
+        public string? Image { get; set; }
+    }
+}

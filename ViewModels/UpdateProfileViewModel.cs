2657a4182c8ff812c5b5a354c2076a0a70ba4aa0
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 22:07:45 +0530

    Update Profile viewmodel

---
+++ ViewModels/UpdateProfileViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fertilizer360.ViewModels
+{
+    public class UpdateProfileViewModel
+    {
+        public string Id { get; set; }  // User ID (hidden in form)
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public bool ChangePassword { get; set; } // Checkbox to toggle password fields
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Current Password")]
+        public string? CurrentPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "New Password")]
+        public string? NewPassword { get; set; }
+    }
+}

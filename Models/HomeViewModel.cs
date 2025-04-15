9f5aa8cce82f042271743e0d32836acffe9902c7
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 21:56:02 +0530

    Home model for view Shops and all

---
+++ Models/HomeViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Fertilizer360.Models
+{
+    public class HomeViewModel
+    {
+        public List<string> Images { get; set; }
+        public List<string> Products { get; set; }
+    }
+}

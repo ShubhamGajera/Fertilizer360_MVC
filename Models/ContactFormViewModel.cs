03ee2f1806caebd96edf40edde9d6c2d8f3fd458
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 21:58:14 +0530

    Conttact model for send all detils of contactus to mail

---
+++ Models/ContactFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Fertilizer360.Models
+{
+    public class ContactFormViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}

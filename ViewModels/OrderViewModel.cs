1e821b2201609a3143c8e809903981379d53f4dd
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 22:08:09 +0530

    Order view model for orders

---
+++ ViewModels/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿// Add to Models folder: OrderViewModel.cs
+namespace Fertilizer360.Models
+{
+    public class OrderViewModel
+    {
+        public int FertilizerId { get; set; }
+        public string FertilizerName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}

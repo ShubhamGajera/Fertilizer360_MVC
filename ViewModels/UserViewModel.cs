7c5902a3d71c8211997fad8b29814e5c62f0caf1
Shubham_Gajera <shubhamgajera122@gmail.com>
2025-04-15 22:07:14 +0530

    For User View model file

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Fertilizer360.ViewModels
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
